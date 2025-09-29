ddaaf1799cf0542d7efa9502085dbdd62c06ed6f
RaulCalderon11 <t01263b@ms.upla.edu.pe>
2025-09-29 01:10:43 -0500

    Add files via upload

---
+++ actividad_01_01/src/main/java/com/mycompany/actividad_01_01/Actividad_01_01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad_01_01;
+
+import vista.Venta_de_Camisetas;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Actividad_01_01 {
+
+    public static void main(String[] args) {
+        Venta_de_Camisetas cami=new Venta_de_Camisetas();
+        cami.show();
+    }
+}
---
+++ actividad_02_01/src/main/java/com/mycompany/actividad_02_01/Actividad_02_01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad_02_01;
+
+import vista.FrmVenderdor;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Actividad_02_01 {
+
+    public static void main(String[] args) {
+        FrmVenderdor vend=new FrmVenderdor();
+        vend.setVisible(true);
+    }
+}
