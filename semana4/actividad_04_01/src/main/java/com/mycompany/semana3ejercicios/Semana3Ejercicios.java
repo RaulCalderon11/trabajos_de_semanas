0e897e9e0efc39f7b9b2f165943bd2c1ccd24c8e
RaulCalderon11 <t01263b@ms.upla.edu.pe>
2025-10-06 01:19:48 -0500

    Add files via upload

---
+++ semana4/actividad_04_01/src/main/java/com/mycompany/semana3ejercicios/Semana3Ejercicios.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana3ejercicios;
+
+import vista.ejercicio1;
+
+/**
+ *
+ * @author WindowsPC
+ */
+public class Semana3Ejercicios {
+
+    public static void main(String[] args) {
+         ejercicio1 pension =new ejercicio1();
+        pension.setVisible(true);
+    }
+}
